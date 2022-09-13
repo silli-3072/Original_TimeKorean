eb93eeabce848212bbe0a4c3ca30ed5b505c4888
MinoriHaruta <hrtmnr.3072@gmail.com>
2022-09-13 12:54:18 +0900

    Initial Commit

---
+++ TimeKorean/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimeKorean
+//
+//  Created by 春田実利 on 2022/09/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

e00ecf4811c2015f7d4c42c148b5af434b74b4e0
MinoriHaruta <hrtmnr.3072@gmail.com>
2022-09-13 14:42:35 +0900

    🏒

---
+++ TimeKorean/Word.swift
@@ -0,0 +1,15 @@
+//
+//  Word.swift
+//  TimeKorean
+//
+//  Created by 春田実利 on 2022/09/13.
+//
+
+import Foundation
+import RealmSwift
+
+class ToDo: Object {
+    @objc dynamic var japanese: String = ""
+    @objc dynamic var Korean: String = ""
+    
+}
---
+++ TimeKorean/WordViewController.swift
@@ -0,0 +1,39 @@
+//
+//  WordViewController.swift
+//  TimeKorean
+//
+//  Created by 春田実利 on 2022/09/13.
+//
+
+import UIKit
+
+class WordViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var wordTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

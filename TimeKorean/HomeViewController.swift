16fb01d395813774bbf623444c5335a02e4272f9
MinoriHaruta <hrtmnr.3072@gmail.com>
2022-09-25 20:06:27 +0900

    🍓

--- TimeKorean/HomeViewController.swift
+++ TimeKorean/HomeViewController.swift
@@ -6,8 +6,11 @@
 //
 
 import UIKit
+import RealmSwift
 
 class HomeViewController: UIViewController {
+    
+    let realm = try! Realm()
     
     @IBOutlet var gameButton: UIButton!
     @IBOutlet var wordButton: UIButton!
@@ -17,8 +20,36 @@ class HomeViewController: UIViewController {
         
         self.gameButton.layer.cornerRadius = 30
         self.wordButton.layer.cornerRadius = 30
-
+        
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func tapGameButton() {
+        let userData = realm.objects(Word.self)
+        
+        if userData.count == 0 {
+            print("🇺🇸")
+            let alert: UIAlertController = UIAlertController(title: "単語の登録がありません", message: "単語を登録してください", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "単語登録", style: .default, handler: { action in
+                self.performSegue(withIdentifier: "WordView", sender: nil)
+            }))
+            
+            present(alert, animated: true, completion: nil)
+        } else if userData.count < 10 {
+            let alert: UIAlertController = UIAlertController(title: "単語の登録が少ないです", message: "単語登録が多い方が高い学習成果を得られます", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "ゲームを続ける", style: .default, handler: { action in
+                self.performSegue(withIdentifier: "toGame", sender: nil)
+            }))
+            
+            alert.addAction(UIAlertAction(title: "単語登録", style: .default, handler: { action in
+                self.performSegue(withIdentifier: "WordView", sender: nil)
+            }))
+            
+            present(alert, animated: true, completion: nil)
+        }
+        
     }
     
 
